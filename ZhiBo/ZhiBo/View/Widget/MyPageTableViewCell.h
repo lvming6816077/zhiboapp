41280b0e3d1a613e95fc223b13567bf5f73f7e13
tennylv <441403517@qq.com>
2016-04-20 21:13:48 +0800

    aa

---
+++ ZhiBo/ZhiBo/View/Widget/MyPageTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MyPageTableViewCell.h
+//  ZhiBo
+//
+//  Created by lvming on 16/4/17.
+//  Copyright © 2016年 tenny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyPageTableViewCell : UITableViewCell
+
+@end
---
+++ ZhiBo/ZhiBo/View/Widget/MyPageTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MyPageTableViewCell.m
+//  ZhiBo
+//
+//  Created by lvming on 16/4/17.
+//  Copyright © 2016年 tenny. All rights reserved.
+//
+
+#import "MyPageTableViewCell.h"
+
+@implementation MyPageTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
